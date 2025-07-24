90805085850c52efafcdfc2482e9481d17f484fb
eter <eter_wheyy@proton.me>
2025-07-24 15:50:25 +0200

    wip

--- src/hash-table.cpp
+++ src/hash-table.cpp
@@ -6,6 +6,7 @@
 
 static const int HT_PRIME_1 = 151;
 static const int HT_PRIME_2 = 163;
+static ht_item HT_DELETED_ITEM = {NULL, NULL};
 
 static ht_item* ht_new_item(const char* k, const char* v)
 {
@@ -63,4 +64,92 @@ static int ht_get_hash(const char* s, const int num_buckets, const int attempt)
   int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
   return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
 }
+
+//To insert a new key-value pair,
+//we iterate through indexes until we find an empty bucket.
+//We then insert the item into that bucket and increment
+//the hash table's count attribute, to indicate a new item has been added.
+
+void ht_insert( ht_hash_table* ht, const char* key, const char* value )
+{
+  ht_item* item = ht_new_item(key, value);
+  int index = ht_get_hash( item->key, ht->size, 0);
+  ht_item* colliding_item = ht->items[index];
+  int i = 1;
+  while ( colliding_item != nullptr )
+  {
+    if ( colliding_item != &HT_DELETED_ITEM )
+    {
+      if (strcmp(colliding_item->key, key) == 0) 
+      {
+        free_ht_item(colliding_item);
+        ht->items[index] = item;
+        return;
+      }
+    }
+    index = ht_get_hash( item->key, ht->size, i);
+    colliding_item = ht->items[index];
+    i++;
+  }
+  ht->items[index] = item;
+  ht->count++;
+}
+
+// Searching is similar to inserting, but at each iteration of the while loop,
+// we check whether the item's key matches the key we're searching for.
+// If it does, we return the item's value. If the while loop hits a NULL bucket,
+// we return NULL, to indicate that no value was found.
+
+char* ht_search( ht_hash_table* ht, const char* key )
+{
+  int index = ht_get_hash( key, ht->size, 0);
+  ht_item* curr_item = ht->items[index];
+  int i = 1;
+  while ( curr_item != nullptr )
+  {
+    if ( curr_item != &HT_DELETED_ITEM )
+    {
+      if ( strcmp(curr_item->key, key) == 0 )
+      {
+        return curr_item->value;
+      }
+    }
+    index = ht_get_hash( key, ht->size, i);
+    curr_item = ht->items[index];
+    i++;
+  }
+  return nullptr;
+}
+
+// Deleting from an open addressed hash table is more complicated than inserting or searching.
+// The item we wish to delete may be part of a collision chain.
+// Removing it from the table will break that chain,
+// and will make finding items in the tail of the chain impossible.
+// To solve this, instead of deleting the item, we simply mark it as deleted.
+// We mark an item as deleted by replacing it with a pointer to a global
+// sentinel item which represents that a bucket contains a deleted item.
+
+void ht_delete( ht_hash_table* ht, const char* key )
+{
+  int index = ht_get_hash( key, ht->size, 0);
+  ht_item* curr_item = ht->items[index];
+  int i = 1;
+  while ( curr_item != nullptr )
+  {
+    if ( curr_item != &HT_DELETED_ITEM)
+    {
+      if ( strcmp(curr_item->key, key) == 0 )
+      {
+        free_ht_item(curr_item);
+        ht->items[index] = &HT_DELETED_ITEM;
+        ht->count--;
+        break;
+      }
+    }
+    index = ht_get_hash( key, ht->size, i);
+    curr_item = ht->items[index];
+    i++;
+  }
+}
+
 
--- src/hash-table.h
+++ src/hash-table.h
@@ -14,5 +14,7 @@ struct ht_hash_table
 };
 
 ht_hash_table* ht_new();
-int ht_hash(const char* s, const int a, unsigned int m);
-void free_ht(ht_hash_table* ht);
+void ht_delete( ht_hash_table* ht, const char* key );
+void ht_insert( ht_hash_table* ht, const char* key, const char* value );
+char* ht_search( ht_hash_table* ht, const char* key );
+void free_ht( ht_hash_table* ht );
